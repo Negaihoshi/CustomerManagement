919f7547c437d18d6ff63074210a9da01526d26c
Negaihoshi <e222et@gmail.com>
2013-11-11 12:13:40 +0800

    Add customer link

--- lib/customerList.php
+++ lib/customerList.php
@@ -63,7 +63,7 @@ echo "<tr><th>ID</th><th>客戶名稱</th><th>信箱</th><th>聯絡電話</th><t
                 $result = mysql_query($sql);
                 while( $row = mysql_fetch_row($result))
                 {
-                   echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td><td>$row[3]</td><td>$row[4]</td><td>$row[5]</td><td>$row[6]</td></tr>";
+                   echo "<tr><td>$row[0]</td><td><a href=''>$row[1]</a></td><td>$row[2]</td><td>$row[3]</td><td>$row[4]</td><td>$row[5]</td><td>$row[6]</td></tr>";
                 }
         }
         else
