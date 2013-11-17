7a14db1d73e96db402fe35ecb119b2545a57d4eb
Negaihoshi <e222et@gmail.com>
2013-11-18 01:23:33 +0800

    AngularJS query done

--- js/customerList.js
+++ js/customerList.js
@@ -5,9 +5,21 @@
 var CustomerList = angular.module('CustomerList', []);
 
 CustomerList.controller('SearchCtrl', function($scope, $http) {
-  $http.get('../json/customers.json').success(function(data) {
+  $http.get('customerDBtoJSON.php').success(function(data) {
     $scope.customers = data;
   });
 
   $scope.orderProp = 'RegisterDate';
 });
+
+/*
+var JsonGet = function($scope, $http){
+	var jsonData = $http({
+		method: 'POST',
+		url: '/json'
+	});
+	jsonData.success(function(response, status, headers, config){
+		$scope.name = response.name;
+	});
+}
+*/
--- lib/customerDBtoJSON.php
+++ lib/customerDBtoJSON.php
@@ -5,11 +5,12 @@
     $result = mysql_query($sql);
     $JsonTable = array();
     while($row = mysql_fetch_array($result)){
-        $JsonTable = array("id"=>"$row[0]","name"=>"$row[1]", "email"=>"$row[2]", "tel"=> "$row[3]", "mobile"=>"$row[4]" ,"address"=>"$row[5]", "RegisterDate"=>"$row[6]");
+        $JsonTable[] = array("id"=>"$row[0]","name"=>"$row[1]", "email"=>"$row[2]", "tel"=> "$row[3]", "mobile"=>"$row[4]" ,"address"=>"$row[5]", "RegisterDate"=>"$row[6]");
     }
     //json_encode($JsonTable, JSON_UNESCAPED_UNICODE);
     echo json_encode($JsonTable, JSON_UNESCAPED_UNICODE);
 
+    /*
     //$Data = json_decode()
     //$json = json_decode($JsonTable);
     echo "<table class='uk-table'><caption>客戶資料</caption><thead>";
@@ -22,4 +23,5 @@
             echo "<tr><td>$row[0]</td><td><a href=''>$row[1]</a></td><td>$row[2]</td><td>$row[3]</td><td>$row[4]</td><td>$row[5]</td><td>$row[6]</td></tr>";
         }
     echo "</tbody></table>";
+    */
 ?>
